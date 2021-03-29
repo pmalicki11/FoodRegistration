9e7789a36d7d12e6a2eee41d3c68ae29ef5252b8
pmalicki11 <pmalicki11@gmail.com>
2021-03-29 11:10:52 +0200

    connection

--- app/config/config.php
+++ app/config/config.php
@@ -4,6 +4,6 @@
   define('DBHOST', '127.0.0.1');
   define('DBNAME', 'food_registration');
   define('DBUSER', 'FoodRegistrationApp');
-  define('DBPASSWORD', 'iwanteatyou');
+  define('DBPASSWORD', 'Iwanteatyou_123');
   define('REMEMBER_ME_COOKIE_NAME', '0249f49bc34492ae07e2f52146c8192b');
   define('COOKIE_DURABILITY', 2630000); // 1 month
