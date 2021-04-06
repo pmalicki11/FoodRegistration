31e44e17225fad4887932bcae3672febdf4d3230
pmalicki11 <pmalicki11@gmail.com>
2021-04-06 06:45:27 +0200

    Axax fixtry5

--- vendor/multiselect-autocomplete/js/multiselect-autocomplete.js
+++ vendor/multiselect-autocomplete/js/multiselect-autocomplete.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
 console.log(document.querySelector('.multiselect-autocomplete'));
   const multiselectAutocomplete = new MultiselectAutocomplete({
     "element": document.querySelector('.multiselect-autocomplete'),
-    "requestURL": "https://localhost/foodregistration/ingredients/ajax?namepart=",
+    "requestURL": "https://pmalicki/foodregistration/ingredients/ajax?namepart=",
     "inputValueAsRequestPart": true,
     "filterRequestDataByInput": false,
     "maxDropdownItems": 5,
